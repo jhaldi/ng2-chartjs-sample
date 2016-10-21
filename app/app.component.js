74df0a4c8db838e704fbf23d6b3fde069fb873a3
John Haldi <john@haldi.net>
2016-10-21 11:40:30 -0400

    add chart to app.component

--- app/app.component.js
+++ app/app.component.js
@@ -9,10 +9,65 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var Chart = require("chart.js");
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(elementRef) {
+        this.elementRef = elementRef;
         this.title = "Bound title string";
+        this.data = {
+            labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    backgroundColor: '#006400'
+                },
+                {
+                    data: [38, 16, 60, 50, 48, 33, 5],
+                    label: 'Series A',
+                    backgroundColor: '#640E1C'
+                },
+                {
+                    data: [28, 48, 40, 19, 86, 27, 90],
+                    label: 'Series B',
+                    backgroundColor: 'pink'
+                }
+            ]
+        };
+        //Chart options
+        this.type = 'bar';
+        this.options = {
+            responsive: true,
+            maintainAspectRatio: false,
+            title: {
+                display: true,
+                text: 'Gross and Net'
+            },
+            legend: {
+                position: 'bottom'
+            },
+            scales: {
+                yAxes: [{
+                        ticks: {
+                            beginAtZero: false
+                        },
+                        stacked: false
+                    }],
+                xAxes: [{
+                        stacked: false
+                    }]
+            }
+        };
     }
+    AppComponent.prototype.ngOnInit = function () {
+        this.c = this.elementRef.nativeElement.querySelector('#test1');
+        this.ctx = this.c.getContext("2d");
+        this.myChart = new Chart(this.ctx, {
+            type: this.type,
+            data: this.data,
+            options: this.options
+        });
+    };
     AppComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
@@ -20,7 +75,7 @@ var AppComponent = (function () {
             templateUrl: 'app.component.html',
             styleUrls: ['app.component.css']
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [core_1.ElementRef])
     ], AppComponent);
     return AppComponent;
 }());
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+
+import Chart = require("chart.js");
 
 @Component({
     moduleId: module.id,
@@ -7,5 +9,72 @@ import { Component } from '@angular/core';
     styleUrls: [ 'app.component.css' ]
 })
 export class AppComponent {
+
     title: string = "Bound title string";
+
+    constructor(private elementRef: ElementRef) { }
+
+    private myChart: any;
+    private c: any;
+    private ctx: any;
+
+    ngOnInit() {
+
+        this.c = this.elementRef.nativeElement.querySelector('#test1');
+        this.ctx = this.c.getContext("2d");
+        this.myChart = new Chart(this.ctx,
+            {
+                type: this.type,
+                data: this.data,
+                options: this.options
+            }
+        );
+
+    }
+
+    data = {
+        labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+        datasets: [
+            {
+                label: "My First dataset",
+                data: [65, 59, 80, 81, 56, 55, 40],
+                backgroundColor: '#006400'
+            },
+            {
+                data: [38, 16, 60, 50, 48, 33, 5],
+                label: 'Series A',
+                backgroundColor: '#640E1C'
+            },
+            {
+                data: [28, 48, 40, 19, 86, 27, 90],
+                label: 'Series B',
+                backgroundColor: 'pink'
+            }
+        ]
+    };
+
+    //Chart options
+    type = 'bar';
+    options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        title: {
+            display: true,
+            text: 'Gross and Net'
+        },
+        legend: {
+            position: 'bottom'
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: false
+                },
+                stacked: false
+            }],
+            xAxes: [{
+                stacked: false
+            }]
+        }
+    };
 }
